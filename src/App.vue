9ebd4499c2586a6eb639c49b7149f914926f61f4
anran <575915485@qq.com>
2023-06-12 15:52:21 +0800

    eslint or prettier

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,50 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-					
-					<button @click="ccc">{{ test }}</button>
+    <button @click="ccc">{{ test }}</button>
   </div>
 </template>
-
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  // components: {
-  //   HelloWorld
-  // },
-	data() {
-		return{
-			test: 'HelloWorld'
-		}
-	},
-	
-		beforeCreate () {console.log('beforeCreate'); },
-		created () { console.log('created');},
-		beforeMount () { console.log('beforeMount');},
-		mount () { console.log('mount');},
-		beforeUpdate () {console.log('beforeUpdate'); },
-		updated () {console.log('updated'); },
-		beforeDestroy () {console.log('beforeDestroy'); },
-		destroy () { console.log('destroy');},
-		methods: {
-			ccc() {
-				console.log('ccc');
-			}
-		}
-}
+  // import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    // components: {
+    //   HelloWorld
+    // },
+    data() {
+      return {
+        test: 'HelloWorld'
+      }
+    },
+    beforeCreate() {
+      console.log('beforeCreate')
+    },
+    created() {
+      console.log('created')
+    },
+    beforeMount() {
+      console.log('beforeMount')
+    },
+    mount() {
+      console.log('mount')
+    },
+    beforeUpdate() {
+      console.log('beforeUpdate')
+    },
+    updated() {
+      console.log('updated')
+    },
+    beforeDestroy() {
+      console.log('beforeDestroy')
+    },
+    destroy() {
+      console.log('destroy')
+    },
+    methods: {
+      ccc() {
+        console.log('ccc')
+      }
+    }
+  }
 </script>
-
-<style>
-</style>
+<style></style>
---
+++ src/index.js
@@ -0,0 +1,3 @@
+let arr = [{ id: '001', name: 'ccc' }]
+console.log(a)
+function test(greeting = 'hello', greeted = '"Wold', silent = false, onMouseOver) {}
