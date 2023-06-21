3c43c2cc6657631e93bacb6292c4ba9efdbe693d
anran <575915485@qq.com>
2023-06-21 15:08:57 +0800

    delete

--- src/index.js
+++
@@ -1,8 +0,0 @@
-let arr = [{ id: '001', name: 'ccc' }]
-// let a = 1
-console.log(a)
-// console.log(arr)
-function test(greeting = 'hello', greeted = '"Wold', silent = false, onMouseOver) {
-  // console.log(greeting, greeted, silent, onMouseOver)
-}
-// test('1', '2', false, '3')
